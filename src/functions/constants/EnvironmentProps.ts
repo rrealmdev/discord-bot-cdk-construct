8674d139f4a2da78deb75f2ebbf3d6b6f897bec2
Tom <tom.runnels@everyrealm.com>
2023-04-12 11:19:05 -0400

    added getDiscordSecretsById function

--- src/functions/constants/EnvironmentProps.ts
+++ src/functions/constants/EnvironmentProps.ts
@@ -1,8 +1,12 @@
 /**
  * The name of the secret key to retrieve.
  */
-export const discordBotAPIKeyName = process.env['DISCORD_BOT_API_KEY_NAME'] ?? 'apiKeyName';
+export const discordBotAPIKeyName = process.env['DISCORD_BOT_API_KEY_NAME'] ?? 'apiKeyName'
+/**
+ * The prefix for the name of the secret key to retrieve.
+ */
+export const discordBotAPIKeyNamePrefix = process.env['DISCORD_BOT_API_KEY_PREFIX'] ?? 'discord-api-key-for-app-'
 /**
  * The ARN for the Discord command lambda.
  */
-export const commandLambdaARN = process.env['COMMAND_LAMBDA_ARN'] ?? 'commandLambdaARN';
+export const commandLambdaARN = process.env['COMMAND_LAMBDA_ARN'] ?? 'commandLambdaARN'
--- src/functions/utils/DiscordSecrets.ts
+++ src/functions/utils/DiscordSecrets.ts
@@ -1,31 +1,51 @@
-import { SecretsManager } from "aws-sdk";
-import { IDiscordSecrets } from "../../types";
-import { discordBotAPIKeyName } from "../constants/EnvironmentProps";
+import { SecretsManager } from 'aws-sdk'
+import { IDiscordSecrets } from '../../types'
+import { discordBotAPIKeyName, discordBotAPIKeyNamePrefix } from '../constants/EnvironmentProps'
 
-const secretsManager = new SecretsManager();
+const secretsManager = new SecretsManager()
 
 /**
  * Cached Discord secrets so we can reduce warm start times.
  */
-let __discordSecrets: IDiscordSecrets | undefined = undefined;
+let __discordSecrets: IDiscordSecrets | undefined = undefined
 
 /**
  * Gets the Discord secrets (public key, client ID, etc.) for use in our lambdas.
- * 
+ *
  * @returns {IDiscordSecrets | undefined} The Discord secrets to be used.
  */
 export async function getDiscordSecrets(): Promise<IDiscordSecrets | undefined> {
   if (!__discordSecrets) {
     try {
-      const discordApiKeys = await secretsManager.getSecretValue({
-        SecretId: discordBotAPIKeyName,
-      }).promise();
+      const discordApiKeys = await secretsManager
+        .getSecretValue({
+          SecretId: discordBotAPIKeyName,
+        })
+        .promise()
       if (discordApiKeys.SecretString) {
-        __discordSecrets = JSON.parse(discordApiKeys.SecretString);
+        __discordSecrets = JSON.parse(discordApiKeys.SecretString)
       }
     } catch (exception) {
-      console.log(`Unable to get Discord secrets: ${exception}`);
+      console.log(`Unable to get Discord secrets: ${exception}`)
     }
   }
-  return __discordSecrets;
-};
+  return __discordSecrets
+}
+
+export async function getDiscordSecretsById(discordApplicationId: string): Promise<IDiscordSecrets | undefined> {
+  if (!__discordSecrets) {
+    try {
+      const discordApiKeys = await secretsManager
+        .getSecretValue({
+          SecretId: `${discordBotAPIKeyNamePrefix}${discordApplicationId}`,
+        })
+        .promise()
+      if (discordApiKeys.SecretString) {
+        __discordSecrets = JSON.parse(discordApiKeys.SecretString)
+      }
+    } catch (exception) {
+      console.log(`Unable to get Discord secrets by id: ${discordApplicationId}: ${exception}`)
+    }
+  }
+  return __discordSecrets
+}
